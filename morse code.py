3e4b7b46a1a6cef090cc3bb49c5e68569642b599
Abdul Salaam Adeleke <78124617+abduladeleke26@users.noreply.github.com>
2024-12-21 18:00:47 -0600

    Add files via upload

---
+++ morse code.py
@@ -0,0 +1,125 @@
+statement = input("What would you like to convert into morse code? ").lower()
+
+letters = list(statement)
+
+morse_code = ""
+
+for i in range(len(letters)):
+    if letters[i] == 'a':
+        morse_code += "·− "
+
+    elif letters[i] == 'b':
+        morse_code += "−··· "
+
+    elif letters[i] == 'c':
+        morse_code += "−·−· "
+
+    elif letters[i] == 'd':
+        morse_code += "−·· "
+
+    elif letters[i] == 'e':
+        morse_code += "· "
+
+    elif letters[i] == 'f':
+        morse_code += "··−· "
+
+    elif letters[i] == 'g':
+        morse_code += "−−· "
+
+    elif letters[i] == 'h':
+        morse_code += "···· "
+
+    elif letters[i] == 'i':
+        morse_code += "·· "
+
+    elif letters[i] == 'j':
+        morse_code += "·−−− "
+
+    elif letters[i] == 'k':
+        morse_code += "−·− "
+
+    elif letters[i] == 'l':
+        morse_code += "·−·· "
+
+    elif letters[i] == 'm':
+        morse_code += "−− "
+
+    elif letters[i] == 'n':
+        morse_code += "−· "
+
+    elif letters[i] == 'o':
+        morse_code += "−−− "
+
+    elif letters[i] == 'p':
+        morse_code += "·−−· "
+
+    elif letters[i] == 'q':
+        morse_code += "−−·− "
+
+    elif letters[i] == 'r':
+        morse_code += "·−· "
+
+    elif letters[i] == 's':
+        morse_code += "··· "
+
+    elif letters[i] == 't':
+        morse_code += "− "
+
+    elif letters[i] == 'u':
+        morse_code += "··− "
+
+    elif letters[i] == 'v':
+        morse_code += "···− "
+
+    elif letters[i] == 'w':
+        morse_code += "·−− "
+
+    elif letters[i] == 'x':
+        morse_code += "−··− "
+
+    elif letters[i] == 'y':
+        morse_code += "−·−− "
+
+    elif letters[i] == 'z':
+        morse_code += "−−·· "
+
+    elif letters[i] == '1':
+        morse_code += "·−−−−  "
+
+    elif letters[i] == '2':
+        morse_code += "··−−−  "
+
+    elif letters[i] == '3':
+        morse_code += "···−−  "
+
+    elif letters[i] == '4':
+        morse_code += "····−  "
+
+    elif letters[i] == '5':
+        morse_code += "·····  "
+
+    elif letters[i] == '6':
+        morse_code += "−····  "
+
+    elif letters[i] == '7':
+        morse_code += "−−···  "
+
+    elif letters[i] == '8':
+        morse_code += "−−−··  "
+
+    elif letters[i] == '9':
+        morse_code += "−−−−·  "
+
+    elif letters[i] == '0':
+        morse_code += "−−−−−  "
+
+
+
+
+
+    elif letters[i] == ' ':
+        morse_code += "  "
+
+
+
+print(morse_code)
